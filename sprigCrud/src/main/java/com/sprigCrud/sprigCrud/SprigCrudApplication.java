f9114d227a44346bfcebd7b9b5ad9f4e628015a6
pasindu <www.pasindudenuka5770@gmail.com>
2024-10-19 11:54:10 +0530

    create project

---
+++ sprigCrud/src/main/java/com/sprigCrud/sprigCrud/SprigCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sprigCrud.sprigCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprigCrudApplication.class, args);
+	}
+
+}
---
+++ sprigCrud/src/test/java/com/sprigCrud/sprigCrud/SprigCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprigCrud.sprigCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
